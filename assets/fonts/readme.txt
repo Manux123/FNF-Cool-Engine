870ba7e67998c95bd808fe20f433fd16232d47b7
Manux123 <manuldestevez@gmail.com>
2021-09-10 20:12:23 -0500

    first commit

---
+++ assets/fonts/readme.txt
@@ -0,0 +1,11 @@
+NOTE: This demo font is FREE for PERSONAL USE ONLY!
+
+By installing or using this font, you are agree to the Product Usage Agreement:
+
+1. This font is only for personal use. No commercial use allowed!
+2. You require a license for promotional or commercial use.
+3. Contact us before any promotional or commercial use!
+
+Email support: creakokunstudio@gmail.com
+
+Thank You
---
+++ source/OptionsData.hx
@@ -0,0 +1,33 @@
+package;
+
+import flixel.FlxG;
+
+class OptionsData
+{
+	public static function initSave()
+		{
+			if (FlxG.save.data.newInput == null)
+				FlxG.save.data.newInput = true;
+	
+			if (FlxG.save.data.downscroll == null)
+				FlxG.save.data.downscroll = false;
+	
+			if (FlxG.save.data.dfjk == null)
+				FlxG.save.data.dfjk = false;
+	
+			if (FlxG.save.data.accuracyDisplay == null)
+				FlxG.save.data.accuracyDisplay = true;
+	
+			if (FlxG.save.data.accuracyDisplay == null)
+				FlxG.save.data.accuracyDisplay = true;
+	
+			if (FlxG.save.data.offset == null)
+				FlxG.save.data.offset = 0;
+	
+			if (FlxG.save.data.offset == null)
+				FlxG.save.data.offset = 0;
+	
+			if (FlxG.save.data.songPosition == null)
+				FlxG.save.data.songPosition = false;
+		}
+}

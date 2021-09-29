d1ef6b9dac32cc737f58e7bcca647b4e5bbeffcf
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2021-09-29 17:09:42 +0200

    Makes the bat file English

--- Installation of the Haxe and APIStuff libraries.bat
+++ Installation of the Haxe and APIStuff libraries.bat
@@ -1,9 +1,9 @@
 @echo off
 title FNF Setup 2.0
-echo Asegurate tener Haxe 4.1.5 y HaxeFlixel instalado!
-echo Presiona cualquier tecla para continuar!
+echo Make sure you have Haxe 4.1.5 and HaxeFlixel installed!
+echo Press any key to continue!
 pause >nul
-title FNF Setup - Instalando librerias
+title FNF Setup - Installing libraries
 echo Instalando librerias haxelib...
 haxelib install lime 7.9.0
 haxelib install openfl 9.0.2
@@ -18,22 +18,22 @@ title FNF Setup
 cls
 haxelib run flixel-tools setup
 cls
-echo Asegurate tener git instalado. Puedes descargarlo en https://git-scm.com/downloads
-echo Presiona cualquier tecla para instalar PolyMod.
+echo Make sure you have Git installed. You can download it from https://git-scm.com/downloads
+echo Press any key to install PolyMod.
 pause >nul
-title FNF Setup - Instalando librerias
+title FNF Setup - Installing libraries
 haxelib git polymod https://github.com/larsiusprime/polymod.git
 cls
-echo Presiona cualquier tecla para instalar Discord_RPC
+echo Press any key to install Discord_RPC
 pause >nul
-title FNF Setup - Instalando librerias
+title FNF Setup - Installing libraries
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 cls
 goto UserActions1
 
 :UserActions1
 title FNF Setup
-set /p menu="Desea instalar la libreria que arregla algunos bugs de transiciones? [Y/N]"
+set /p menu="Do you wanna install a library that fixes some transitioning bug? [Y/N]"
        if %menu%==Y goto FixTransitionBug
        if %menu%==y goto FixTransitionBug
        if %menu%==N goto UserActions2
@@ -43,7 +43,7 @@ set /p menu="Desea instalar la libreria que arregla algunos bugs de transiciones
 :UserActions2
 cls
 title FNF Setup
-set /p menu2="Quieres que yo cree el archivo APIStuff.hx de forma automatica? [Y/N]"
+set /p menu2="Do you want I create APIStuff.hx automatically? [Y/N]"
        if %menu2%==Y goto APIStuffYes
        if %menu2%==y goto APIStuffYes
        if %menu2%==N goto APIStuffNo
@@ -64,18 +64,18 @@ echo }
 cd ..
 cls
 title FNF Setup 
-echo Setup completo. Presione cualquier tecla para salir.
+echo Setup complete. Press any key to exit.
 pause >nul
 exit
 
 :APIStuffNo
 cls
 title FNF Setup
-echo Setup completo. Presione cualquier tecla para salir.
+echo Setup completo. Press any key to exit.
 pause >nul
 exit
 
 :FixTransitionBug
-title FNF Setup - Instalando librerias
+title FNF Setup - Installing libraries
 haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
 goto UserActions2

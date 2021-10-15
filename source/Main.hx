d7949c8058969889332ee4ab5e483243dbd2a263
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2021-10-15 22:33:43 +0200

    yes? idk

--- source/Main.hx
+++ source/Main.hx
@@ -66,6 +66,8 @@ class Main extends Sprite
 
 		#if !debug
 		initialState = states.LoadingStartState;
+		#else
+		initialState = states.TitleState;
 		#end
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));

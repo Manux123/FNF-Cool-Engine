7c13369876f1ceda48ed5f8a2e709df7a2fb67d4
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2021-11-03 19:01:22 +0100

    misspell

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2051,7 +2051,7 @@ class PlayState extends states.MusicBeatState
 					ss = false;
 					goods++;
 				}
-				else if (noteInput > Conductor.safeZoneOffset * 0.2 || noteInput > Conductor.safeZoneOffset * 0.1)  //well, sick are stuck and then now works how the RatingType good, bad, shit.
+				else if (noteInput > Conductor.safeZoneOffset * 0.2 || noteInput > Conductor.safeZoneOffset * 0.1)  //well, sick are stuck and then now works like the RatingType good, bad, shit.
 				{
 					RatingType = 'sick';
 					sicks++;

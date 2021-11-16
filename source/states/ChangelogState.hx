672a773e9e565e5eaaa33ab28917e55e017b4937
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2021-11-17 01:55:09 +0300

    Create ChangelogState.hx

---
+++ source/states/ChangelogState.hx
@@ -0,0 +1,40 @@
+package states;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class ChangelogState extends states.MusicBeatState
+{
+	
+  public static var daChangelogNeeded:String = "If i knew the lastest feature i'll say it, i promise";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var ver = Application.current.meta.get('version');
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"Features: "
+			+ daChangelogNeeded		      
+			+ "Press backspace to return",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+  	override function update(elapsed:Float)
+	{
+    
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}

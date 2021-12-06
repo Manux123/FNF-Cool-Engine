fc1c51b7bf9e34d7e9fd524d94da47433c2fb97f
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2021-12-07 01:36:25 +0300

    Create GJKeys.hx

---
+++ source/states/GJKeys.hx
@@ -0,0 +1,6 @@
+package;
+class GJKeys
+{
+    public static var id:Int = ; // since the engine still isnt on game jolt idk the id
+    public static var key:String = ""; // secret api key but same with the up one idk cuz no gamejolt yet
+}

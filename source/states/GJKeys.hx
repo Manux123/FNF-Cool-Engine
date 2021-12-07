8eafaf7023a745d8c96e7adaf6901fd25ed38929
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2021-12-07 15:08:26 +0300

    ayo

--- source/states/GJKeys.hx
+++ source/states/GJKeys.hx
@@ -1,6 +1,6 @@
 package states;
 class GJKeys
 {
-    public static var id:Int = ; // since the engine still isnt on game jolt idk the id
-    public static var key:String = ""; // secret api key but same with the up one idk cuz no gamejolt yet
+    public static var id:Int = "669888"; // id
+    public static var key:String = "64b6c987a03b4f3bfae110553b8a496a"; // secret api key
 }

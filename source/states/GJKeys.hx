33b8e20e558381bd6ea071516b3f5a33a11667e4
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2021-12-07 18:50:04 +0300

    Update GJKeys.hx

--- source/states/GJKeys.hx
+++ source/states/GJKeys.hx
@@ -1,6 +1,6 @@
 package states;
 class GJKeys
 {
-    public static var id:Int = "669888"; // id
+    public static var id:Int = 669888; // id
     public static var key:String = "64b6c987a03b4f3bfae110553b8a496a"; // secret api key
 }

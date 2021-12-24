9aab6d85ae78bbb9115baf6b6ca9b6824862d457
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2021-12-24 19:45:47 +0300

    credits thing lol

--- source/states/OptionsMenuState.hx
+++ source/states/OptionsMenuState.hx
@@ -45,6 +45,7 @@ class OptionsMenuState extends MusicBeatState
 			("\n" + 'Optimization') +
 			("\n" + 'Note Skin') +
 			("\n" + 'Controls') +
+			("\n" + 'Credits') +
 			("\n" + 'Exit'));
 		
 		//trace(controlsStrings);
@@ -135,6 +136,8 @@ class OptionsMenuState extends MusicBeatState
 						FlxG.state.openSubState(new KeyBindMenu());
 						#end
 					case 5:
+						FlxG.switchstate(new CreditsState());
+					case 6:
 						FlxG.switchState(new MainMenuState());
 				}
 			}

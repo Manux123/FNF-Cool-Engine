499d983cbbeb6dc75d37799a6987968f55d98307
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2022-01-01 02:28:55 +0300

    GAMEJOLT

---
+++ source/GJKeys.hx
@@ -0,0 +1,6 @@
+package;
+class GJKeys
+{
+    public static var id:Int = 	676101; // Put your game's ID here
+    public static var key:String = "77e6184dff023dcb69145df4375b753a"; // Put your game's private API key here
+}

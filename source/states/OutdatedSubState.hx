46cfbe89f7a3b119f7750bdee94f276254581c9d
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2022-01-18 21:20:10 +0300

    gonna make things spooky when i find out how to make it find the date so ye

--- source/states/OutdatedSubState.hx
+++ source/states/OutdatedSubState.hx
@@ -13,16 +13,44 @@ class OutdatedSubState extends states.MusicBeatState
 
 	public static var daVersionNeeded:String = "If i knew the lastest version i'll say it, i promise";
 	
-    public static var daChangelogNeeded:String = "If i knew the lastests features i'll say it, i promise";
-
+        public static var daChangelogNeeded:String = "If i knew the lastests features i'll say it, i promise";
+	
+	#if sys       
+		// i wanna make things spooky so ill make it so if the date is october 31 it will show your computer name
+		function getComputerName():String {
+		
+			var env = Sys.environment();
+			if (!env.exists("COMPUTERNAME")) {
+				return null;
+			}
+			return env["COMPUTERNAME"];
+		}
+        #end
+	
 	override function create()
 	{
+		
 		super.create();
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 		var ver = Application.current.meta.get('version');
-		var txt:FlxText = new FlxText(0, 0, FlxG.width,
-			"HEY! You're running an outdated version of the Cool Engine!\nYour current version is "
+		/*#if sys
+		var txtHalloween:FlxText = new FlxText(0, 0, FlxG.width,
+			"HEY!"  + getComputerName "Your running an outdated version of the Cool Engine!\nYour current version is "
+			+ ver
+			+ " while the most recent version is "
+			+ daVersionNeeded
+			+ " here are the features youre missing on\n"
+			+ daChangelogNeeded		      
+			+ "\n Press Space to go the GitHub page, or ESCAPE to ignore this.",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txtHalloween);
+	}
+	#else*/
+	        var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"HEY! Your running an outdated version of the Cool Engine!\nYour current version is "
 			+ ver
 			+ " while the most recent version is "
 			+ daVersionNeeded
@@ -34,6 +62,7 @@ class OutdatedSubState extends states.MusicBeatState
 		txt.screenCenter();
 		add(txt);
 	}
+           //#end
 
 	override function update(elapsed:Float)
 	{

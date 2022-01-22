1ff81e10d7534cc9633b48ff5e799bc06e7dfee3
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2022-01-22 16:46:04 +0300

    more text for funni

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -348,8 +348,8 @@ class TitleState extends states.MusicBeatState
 		}
 	}
 
-	var randomString = ['Thx PabloelproxD210','Thx Chase for...',"Thx TheStrexx for", "YOU WHAT", "We shouldn't be"];
-	var randomString2 = ['for the Android port LOL','SOMTHING',"you'r 3 commits :D", "*dances epicly*", "hardcoding this lmao"];
+	var randomString = ['Thx PabloelproxD210','Thx Chase for...',"Thx TheStrexx for", "YOU WHAT", "We shouldn't be", "you sussy baka", "._.", "get a brain", "sus", "we are cool"];
+	var randomString2 = ['for the Android port LOL','SOMTHING',"you'r 3 commits :D", "*dances epicly*", "hardcoding this lmao", "among us", "im not imposter i swear", "for me", "sus", "right?";
 	var random:Int;
 
 	override function beatHit()

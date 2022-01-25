8efb848c75ca27067c6235300af8901fa63a8e66
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2022-01-25 20:04:25 +0300

    shut
    @susyboy23

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -359,9 +359,14 @@ class TitleState extends states.MusicBeatState
 	{
 		super.beatHit();
 
-		randomString = ['Thx PabloelproxD210','Thx Chase for...',"Thx TheStrexx for", userName()];//This is for credits, not for funny texts :angry:
-		randomString2 = ['for the Android port LOL','SOMTHING',"you'r 3 commits :D", "Thanks for playing B)"]; //But can't we use the txt file version instead of this hardcoded ver? :cries:
-
+		/*randomString = ['Thx PabloelproxD210','Thx Chase for...',"Thx TheStrexx for", userName()];//This is for credits, not for funny texts :angry:
+		randomString2 = ['for the Android port LOL','SOMTHING',"you'r 3 commits :D", "Thanks for playing B)"];*/ //But can't we use the txt file version instead of this hardcoded ver? :cries:
+                // shut up with your bing chingling overcharged we already put every fucking person that helped with the mod
+		// also jaunen we use the hard coded one because its hard :troll:
+		
+		randomString = ['Thx PabloelproxD210','Thx Chase for...',"Thx TheStrexx for", "YOU WHAT", "We shouldn't be", "you sussy baka", "._.", "get a brain", "sus", "we are cool", userName(), "why are we", userName()];
+		randomString2 = ['for the Android port LOL','SOMTHING',"you'r 3 commits :D", "*dances epicly*", "hardcoding this lmao", "among us", "im not imposter i swear", "for me", "sus", "right?", "run", "hardcoding this", "thanks for playing"];
+		
 		logoBl.animation.play('bump');
 		danceLeft = !danceLeft;
 

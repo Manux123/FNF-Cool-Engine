90586f7416b605c694a9fc1c98e2ee59791d75d3
YumDev21 <103183001+YumDev21@users.noreply.github.com>
2022-04-12 22:07:49 +0100

    I thought this might look better
    You dont have to

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -99,6 +99,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
 			//menuItem.screenCenter(X);
+			menuItem.x = 100;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
